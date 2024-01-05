08b8002f970f66daebe862b195e1adcd274d4181
Sanket Kumar <79749102+Sanketkmr1801@users.noreply.github.com>
2024-01-05 23:56:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -509,4 +509,8 @@ def formatTime(dt):
 
   return dt
 
-# update_db()
+if __name__ == "__main__":
+    # asyncio.run(schedule_update_db()) 
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+    pass
